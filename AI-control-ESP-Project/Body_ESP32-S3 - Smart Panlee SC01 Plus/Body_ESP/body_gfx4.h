93835955625e3d765fcb4b6e9561903118e92676
Nonaak <nonaak43@hotmail.com>
2025-10-25 10:44:30 +0200

    Add files via upload

---
+++ AI-control-ESP-Project/Body_ESP32-S3 - Smart Panlee SC01 Plus/Body_ESP/body_gfx4.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <Arduino.h>
+#include "body_config.h"
+
+class Arduino_GFX; 
+extern Arduino_GFX *body_gfx;
+
+enum { G4_HART=0, G4_HUID=1, G4_TEMP=2, G4_ADEMHALING=3, G4_OXY=4, G4_HOOFDESP=5, G4_ZUIGEN=6, G4_TRIL=7, G4_NUM=8 };
+
+// Body GFX4 interface with HoofdESP styling
+void body_gfx4_begin();
+void body_gfx4_clear();
+void body_gfx4_setLabel(uint8_t ch, const char* name);
+void body_gfx4_pushSample(uint8_t ch, float value, bool mark=false);
+void body_gfx4_drawButtons(bool recording, bool playing, bool menuActive, bool overruleActive, bool recDisabled=false);
+void body_gfx4_drawStatus(const char* text);
+
+// Touch cooldown management
+void body_gfx4_setCooldown(uint32_t mainScreenMs, uint32_t menuMs);
+bool body_gfx4_canTouch(bool isMainScreen);
+void body_gfx4_registerTouch(bool isMainScreen);
