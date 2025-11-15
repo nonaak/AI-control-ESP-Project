4ed8918781bccdd2856ac3d001d7ef4d97db92b4
Nonaak <nonaak43@hotmail.com>
2025-11-15 10:31:55 +0100

    up

--- AI-control-ESP-Project/Hooft_ESP/Hooft_ESP_KEON/ui.cpp
+++ AI-control-ESP-Project/Hooft_ESP/Hooft_ESP_KEON/ui.cpp
@@ -1524,7 +1524,7 @@ if (LCD_BL >= 0) {
 }
 
   // // cv->begin() causes RGB panel slot conflict - commented for Jingcai RGB
-  // cv->begin();
+  cv->begin();
   cv->fillScreen(CFG.COL_BG); cv->flush();
 
   uiMode  = MODE_MENU;
